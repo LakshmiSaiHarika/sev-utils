9d781d000e2f62e855b389edea1ad966aba80b68
LakshmiSaiHarika <113560385+LakshmiSaiHarika@users.noreply.github.com>
2024-05-09 00:20:27 -0700

    change back to previous commit

--- tools/snp.sh
+++ tools/snp.sh
@@ -93,7 +93,6 @@ GENERATED_INITRD_BIN="${SETUP_WORKING_DIR}/initrd.img"
 # Redhat Sensitive Credentials -- User must specify, no default value set
 RHEL_SUBS_MGR_USER="amd"
 RHEL_SUBS_MGR_PASS="amd"
-source "$HOME/.bash_profile"
 REDHAT_OFFLINE_TOKEN="${REDHAT_OFFLINE_TOKEN}"
 
 
